e9dd57b7c6d1af4c1848b7806d52ba4f53b279d7
Nick Lane-Smith <nlanesmith@gmail.com>
2013-08-09 00:20:41 -0700

    adding rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackboneRailsHandlebarsExample::Application.config.secret_key_base = '56ff5710f1c4479146bb4270e85239c4966ce2ef3ba0f5b81d2e001bbc57f517057a2a454552193b2dd3ba119bb014af3c97855a7920999e866489d462cc49b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BackboneRailsHandlebarsExample::Application.config.session_store :cookie_store, key: '_backbone-rails-handlebars-example_session'
